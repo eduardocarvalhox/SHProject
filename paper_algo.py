e4551122c6c020820aa6bcf8ac26fb978f2f63c3
Eduardo <eduardocarvalhox@gmail.com>
2017-01-10 11:56:38 +0100

    Start paper_algo and add first example

--- paper_algo.py
+++ paper_algo.py
@@ -1,1 +1,13 @@
-import 
+import pandas as pd
+
+#Open file
+filename = "example1.txt"
+txt = open(filename)
+
+#Read the number of cities
+txt.readline()
+txt.readline()
+line = txt.readline()
+ncities = line.split()[1]
+
+
