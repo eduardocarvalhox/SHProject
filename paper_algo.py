3e754bb6fbbffb25a5ecbcba881b401e9be3de1e
Eduardo <eduardocarvalhox@gmail.com>
2017-01-10 12:16:24 +0100

    Read the cities table

--- paper_algo.py
+++ paper_algo.py
@@ -8,6 +8,9 @@ txt = open(filename)
 txt.readline()
 txt.readline()
 line = txt.readline()
-ncities = line.split()[1]
+ncities = int(line.split()[1])
 
+#Read the table of cities' positions
+cities_positions = pd.read_csv('example1.txt', sep='\t', header = None, skiprows = 10, nrows = ncities)
+cities_positions.columns = ["City", "X", "Y"]
 
